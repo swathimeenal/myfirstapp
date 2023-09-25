f7410d1335e3261b24d6e295ed280e51eb0edc3e
swathimeenal <swathimeenal1994@gmail.com>
2023-09-26 02:01:31 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import Card from './Card.jsx'
 function App() {
-
-  return (
-    <>
-      <section className="pricing py-5">
+  const data =[
+    {
+    plan:"FREE",
+    price:"0",
+    user:"Single User",
+    userEnabler:true,
+    storage:"5GB Storage",
+    storageEnabler:true,
+    publicProjects:"Unlimited Public Projects",
+    publicProjectsEnabler:true,
+    communityAccess:"Community Access",
+    communityAccessEnabler:true,
+    privateProjects:"Unlimited Private Projects",
+    privateProjectsEnabler:false,
+    support:"Dedicated Phone Support",
+    supportEnabler:false,
+    subDomain:"Free Subdomain",
+    subDomainEnabler:false,
+    reports:"Monthly Status Reports",
+    reportsEnabler:false
+   },
+    {
+      plan:"PLUS",
+      price:"9",
+      user:<b>"5 User"</b>,
+      userEnabler:true,
+      storage:"5GB Storage",
+      storageEnabler:true,
+      publicProjects:"Unlimited Public Projects",
+      publicProjectsEnabler:true,
+      communityAccess:"Community Access",
+      communityAccessEnabler:true,
+      privateProjects:"Unlimited Private Projects",
+      privateProjectsEnabler:true,
+      support:"Dedicated Phone Support",
+      supportEnabler:true,
+      subDomain:"Free Subdomain",
+      subDomainEnabler:true,
+      reports:"Monthly Status Reports",
+      reportsEnabler:false
+      },
+      {
+        plan:"PRO",
+        price:"49",
+        user:<b>"Unlimited User"</b>,
+        userEnabler:true,
+        storage:"5GB Storage",
+        storageEnabler:true,
+        publicProjects:"Unlimited Public Projects",
+        publicProjectsEnabler:true,
+        communityAccess:"Community Access",
+        communityAccessEnabler:true,
+        privateProjects:"Unlimited Private Projects",
+        privateProjectsEnabler:true,
+        support:"Dedicated Phone Support",
+        supportEnabler:true,
+        subDomain:<b>"Unlimited Free Subdomain"</b>,
+        subDomainEnabler:true,
+        reports:"Monthly Status Reports",
+        reportsEnabler:true
+        }
+]
+return (
+ <>
+  <section className="pricing py-5">
   <div className="container">
     <div className="row">
-     {/* <!-- Free Tier -->*/}
-      <div className="col-lg-4">
-        <div className="card mb-5 mb-lg-0">
-          <div className="card-body">
-            <h5 className="card-title text-muted text-uppercase text-center">Free</h5>
-            <h6 className="card-price text-center">$0<span className="period">/month</span></h6>
-            <hr/>
-            <ul className="fa-ul">
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Single User</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>5GB Storage</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
-              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Unlimited
-                Private Projects</li>
-              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Dedicated
-                Phone Support</li>
-              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Free Subdomain
-              </li>
-              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Monthly Status
-                Reports</li>
-            </ul>
-            <div className="d-grid">
-              <a href="#" className="btn btn-primary text-uppercase">Button</a>
-            </div>
-          </div>
-        </div>
-      </div>
-       {/*<!-- Plus Tier --> */}
-      <div className="col-lg-4">
-        <div className="card mb-5 mb-lg-0">
-          <div className="card-body">
-            <h5 className="card-title text-muted text-uppercase text-center">Plus</h5>
-            <h6 className="card-price text-center">$9<span className="period">/month</span></h6>
-            <hr/>
-            <ul className="fa-ul">
-              <li><span className="fa-li"><i className="fas fa-check"></i></span><strong>5 Users</strong></li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>50GB Storage</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Private Projects</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Dedicated Phone Support</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Free Subdomain</li>
-              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Monthly Status
-                Reports</li>
-            </ul>
-            <div className="d-grid">
-              <a href="#" className="btn btn-primary text-uppercase">Button</a>
-            </div>
-          </div>
-        </div>
-      </div>
-     {/*<!-- Pro Tier --> */}
-      <div className="col-lg-4">
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title text-muted text-uppercase text-center">Pro</h5>
-            <h6 className="card-price text-center">$49<span className="period">/month</span></h6>
-            <hr/>
-            <ul className="fa-ul">
-              <li><span className="fa-li"><i className="fas fa-check"></i></span><strong>Unlimited Users</strong>
-              </li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>150GB Storage</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Private Projects</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Dedicated Phone Support</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span><strong>Unlimited</strong> Free
-                Subdomains</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Monthly Status Reports</li>
-            </ul>
-            <div className="d-grid">
-              <a href="#" className="btn btn-primary text-uppercase">Button</a>
-            </div>
-          </div>
-        </div>
+      {
+        data.map((e,i)=>{ 
+          return <Card data={e} key={i}/>
+          {/* sending data to card component */}
+          
+         })
+      }
       </div>
     </div>
-  </div>
 </section>
-    </>
-  )
+ </> 
+ )
 }
+
 
 export default App
